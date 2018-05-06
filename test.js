e678a335912105831385e70ae3532d7795d82972
1aby <1abyr@naver.com>
2018-05-06 18:25:41 +0900

    test 파일 수정

--- test.js
+++ test.js
@@ -5,6 +5,13 @@ var input_stdin = "";
 var input_stdin_array = "";
 var input_currentline = 0;
 
+var fs = require('fs');
+fs.readFile('abc.txt','utf8',function(error,data){
+    input_stdin += data;
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
 process.stdin.on('data', function (data) {
     input_stdin += data;
 });
@@ -19,14 +26,14 @@ function readLine() {
 }
 function solveMeFirst(a, b) {
   // Hint: Type return a+b below   
-  var c = a+b;
+  
     return a+b;
 }
 
-//function main() {
+function main() {
     var a = parseInt(readLine());
     var b = parseInt(readLine());
 
     var res = solveMeFirst(a, b);
     console.log(res);
-//}
+}
